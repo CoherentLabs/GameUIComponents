2ec0aa618c17eb05a6ad6902adbf5e59557b2f1b
venetak <veneta.kochovska93@gmail.com>
2021-02-16 08:32:25 +0200

    Add templates to templates file

--- components/router/demo/demo.js
+++ components/router/demo/demo.js
@@ -2,6 +2,18 @@ import components from 'coherent-gameface-components';
 import {Router, Route, BrowserHistory} from '../umd/router.development.js';
 import { pm } from 'postmessage-polyfill';
 import { fetch as fetchPolyfill } from 'whatwg-fetch';
+import {
+    tanksTemplate,
+    healersTemplate,
+    startGameTemplate,
+    homeTemplate,
+    heroesTemplate,
+    tankOneTemplate,
+    tankTwoTemplate,
+    tankThreeTemplate,
+    healerTemplate,
+    notFoundTemplate,
+} from './templates';
 
 const browserHistory = new BrowserHistory();
 Route.use(browserHistory);
@@ -17,7 +29,7 @@ window.postMessage = function (message) {
 class Home extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div>Home</div>`;
+        this.template = homeTemplate;
     }
 
     connectedCallback() {
@@ -34,7 +46,7 @@ class Home extends HTMLElement {
 class StartGame extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div>Start Game</div>`;
+        this.template = startGameTemplate;
     }
 
     connectedCallback() {
@@ -85,24 +97,7 @@ class Healers extends HTMLElement {
     `;
         
         components.importStyleTag('supports', style);
-        this.template = `<div>    <div>Available Supports:</div>    <p></p>    <div class="healers-menu">        <gameface-route slot="route" to="/heroes/healers/paladin">            <div class="avatar-container">                <div class="avatar avatar1"></div>                <div>Paladin</div>            </div>        </gameface-route>        <gameface-route slot="route" to="/heroes/healers/monk">            <div class="avatar-container">                <div class="avatar avatar2"></div>                <div>Monk</div>            </div>        </gameface-route>        <gameface-route slot="route" to="/heroes/healers/priest">            <div class="avatar-container">                <div class="avatar avatar3"></div>                <div>Priest</div>            </div>        </gameface-route>    </div></div>`;
-    }
-
-    connectedCallback() {
-        components.loadResource(this)
-            .then(([loadedTemplate]) => {
-                this.template = loadedTemplate;
-
-                components.renderOnce(this);
-            })
-            .catch(err => console.error(err));
-    }
-}
-
-class DPS extends HTMLElement {
-    constructor() {
-        super();
-        this.template = `<div>DPS</div>`;
+        this.template = healersTemplate;
     }
 
     connectedCallback() {
@@ -139,20 +134,20 @@ class Tanks extends HTMLElement {
                 background-size: contain;
                 background-repeat: no-repeat no-repeat;
             }
-            .avatar1 {
+            .tank-avatar1 {
                 background-image: url(/images/avatar1.png);
             }
 
-            .avatar2 {
+            .tank-avatar2 {
                 background-image: url(/images/avatar4.png);
             }
-            .avatar3 {
+            .tank-avatar3 {
                 background-image: url(/images/avatar5.png);
             }
         `;
 
         components.importStyleTag('tanks', style);
-        this.template = `<div>    <div>Available Tanks:</div>    <p></p>    <div class="tanks-menu">        <gameface-route slot="route" to="/heroes/tanks/one/79/12">            <div class="avatar-container">                <div class="avatar avatar1"></div>                <div>Tank 1</div>            </div>        </gameface-route>        <gameface-route slot="route" to="/heroes/tanks/two">            <div class="avatar-container">                <div class="avatar avatar2"></div>                <div>Tank 2</div>            </div>        </gameface-route>        <gameface-route slot="route" to="/heroes/tanks/three">            <div class="avatar-container">                <div class="avatar avatar3"></div>                <div>Tank 3</div>            </div>        </gameface-route>    </div></div>`;
+        this.template = tanksTemplate;
     }
 
     connectedCallback() {
@@ -169,7 +164,7 @@ class Tanks extends HTMLElement {
 class Heroes extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div><div>Heroes:</div><div class="menu"><gameface-route slot="route" to="/heroes/tanks">Tanks</gameface-route><gameface-route slot="route" to="/heroes/healers">Healers</gameface-route><gameface-route slot="route" to="/heroes/dps">Damage Dealers</gameface-route></div><router-view></router-view></div>`;
+        this.template = heroesTemplate;
     }
 
     connectedCallback() {
@@ -185,7 +180,7 @@ class Heroes extends HTMLElement {
 class TankOne extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div><div>Tank One:</div><p>Tank One is the very first tank in the Evergloom. It uses its roots to defend the realm of trees.</p></div>`;
+        this.template = tankOneTemplate;
     }
 
     connectedCallback() {
@@ -201,7 +196,7 @@ class TankOne extends HTMLElement {
 class TankTwo extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div><div>Tank Two:</div><p>Tank Two is the very first tank in the Evergloom. It uses its roots to defend the realm of trees.</p></div>`;
+        this.template = tankTwoTemplate;
     }
 
     connectedCallback() {
@@ -217,7 +212,7 @@ class TankTwo extends HTMLElement {
 class TankThree extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div><div>Tank Three:</div><p>Tank Three is the very first tank in the Evergloom. It uses its roots to defend the realm of trees.</p></div>`;
+        this.template = tankThreeTemplate;
     }
 
     connectedCallback() {
@@ -236,21 +231,21 @@ class Healer extends HTMLElement {
 
         this.healers = {
             'priest': {
-                name: 'Farondis',
+                mana: 200,
                 strength: 100
             },
             'paladin': {
-                name: 'Flynn',
+                mana: 100,
                 strength: 90
             },
             'monk': {
-                name: 'Cho',
+                mana: 300,
                 strength: 110
             },
         }
 
         
-        this.template = `<div><div>Name: <span id="name"></span></div><div>Strength: <span id="strength"></span></div></div>`;
+        this.template = healerTemplate;
     }
 
     connectedCallback() {
@@ -260,7 +255,7 @@ class Healer extends HTMLElement {
         components.loadResource(this)
             .then(([loadedTemplate]) => {
                 this.template = loadedTemplate;
-                this.template.querySelector('#name').textContent = this.model.name;
+                this.template.querySelector('#mana').textContent = this.model.mana;
                 this.template.querySelector('#strength').textContent = this.model.strength;
                 components.renderOnce(this);
             })
@@ -272,7 +267,7 @@ class Healer extends HTMLElement {
 class NotFound extends HTMLElement {
     constructor() {
         super();
-        this.template = `<div>404</div>`;
+        this.template = notFoundTemplate;
     }
 
     connectedCallback() {
@@ -292,7 +287,6 @@ components.defineCustomElement('heroes-page', Heroes);
 components.defineCustomElement('tanks-page', Tanks);
 components.defineCustomElement('healers-page', Healers);
 components.defineCustomElement('healer-page', Healer);
-components.defineCustomElement('dps-page', DPS);
 components.defineCustomElement('tank-one-page', TankOne);
 components.defineCustomElement('tank-two-page', TankTwo);
 components.defineCustomElement('tank-three-page', TankThree);
@@ -306,7 +300,6 @@ let tanksRouter = new Router({
 });
 
 let heroesRouter = new Router({
-    '/dps': 'dps-page',
     '/healers': 'healers-page',
     '/healers/:id': 'healer-page',
     '/tanks': 'tanks-page',
