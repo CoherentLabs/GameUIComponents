45381a552c31d570846599dc7f62970fdf13604d
venetak <veneta.kochovska93@gmail.com>
2021-02-22 17:38:44 +0200

    Add hidden class, select first option by default and clean demo file Fix styles Added styles

--- components/dropdown/README.md
+++ components/dropdown/README.md
@@ -16,13 +16,32 @@ The gameface-dropdown component comes with UMD and CJS builds.
 * import the gameface-dropdown component:
 
 ~~~~{.html}
-<script src="./node_modules/gameface-dropdown/umd/gameface-dropdown.production.min.js"></script>
+<script src="./node_modules/gameface-dropdown/umd/dropdown.production.min.js"></script>
 ~~~~
 
 * add the gameface-dropdown component to your html:
 
 ~~~~{.html}
-<gameface-gameface-dropdown class="gameface-dropdown-component"></gameface-gameface-dropdown>
+<gameface-dropdown></gameface-dropdown>
+~~~~
+
+To add options to the dropdown use the **option** slot:
+
+~~~~{.html}
+<gameface-dropdown id="my-dropdown">
+    <dropdown-option slot="option">Cat</dropdown-option>
+    <dropdown-option slot="option">Dog</dropdown-option>
+    <dropdown-option slot="option">Giraffe</dropdown-option>
+    <dropdown-option slot="option">Lion</dropdown-option>
+</gameface-dropdown>
+~~~~
+
+To manually select an option use the setSelected(`<optionIndex>`) function.
+For example - to select the Giraffe option from the dropdown above call:
+
+~~~~{.js}
+const dropdown = document.getElementById('my-dropdown');
+dropdown.setSelected(2);
 ~~~~
 
 This is all! Load the file in Gameface to see the gameface-dropdown.
@@ -32,7 +51,7 @@ which import the components and the gameface-dropdown from the node_modules fold
 
 ~~~~{.js}
 import components from 'coherent-gameface-components';
-import gameface-dropdown from 'gameface-dropdown';
+import GamefaceDropdown from 'gameface-dropdown';
 ~~~~
 
 Note that this approach requires a module bundler like [Webpack](https://webpack.js.org/) or [Rollup](https://rollupjs.org/guide/en/) to resolve the
@@ -40,7 +59,7 @@ modules from the node_modules folder. Alternatively you can import them directly
 
 ~~~~{.js}
 import components from './node_modules/coherent-gameface-components/umd/components.production.min.js';
-import gameface-dropdown from './node_modules/gameface-dropdown/umd/gameface-dropdown.production.min.js';
+import GamefaceDropdown from './node_modules/gameface-dropdown/umd/dropdown.production.min.js';
 ~~~~
 
 ## Usage with CJS modules:
@@ -49,7 +68,7 @@ import gameface-dropdown from './node_modules/gameface-dropdown/umd/gameface-dro
 
 ~~~~{.js}
 const components = require('coherent-gameface-components');
-const gameface-dropdown = require('gameface-dropdown');
+const GamefaceDropdown = require('gameface-dropdown');
 ~~~~
 
 The CommonJS(CJS) modules are used in a NodeJS environment, be sure to use a module
--- components/dropdown/script.js
+++ components/dropdown/script.js
@@ -21,73 +21,50 @@ class GamefaceDropdown extends HTMLElement {
 
         this.selectedOption = null;
         this.isOpened = false;
+        // the index of the currently selected option element
         this._selected = 0;
 
         this.template = template;
         components.importStyleTag('gameface-dropdown', style);
-        this.url = '/components/gameface-dropdown/template.html';
-
-        this.onClick = this.onClick.bind(this);
     }
 
+    /**
+     * Returns all dropdown-option elements.
+     * @returns {Array<HTMLElement>}
+    */
+    get allOptions() {
+        return this.querySelectorAll('dropdown-option');
+    }
+
+    /**
+     * Sets the currently selected option.
+     * Updates the class names of the current and the previously active options.
+     * Dispatches a change event to notify that the active option has been changed.
+    */
     set selected(option) {
-        // TODO: fix
         if (this._selected) {
-            this.querySelectorAll('dropdown-option')[this._selected].style.backgroundColor = '#fff'
+            this.allOptions[this._selected].classList.remove('active');
         }
 
-        option.style.backgroundColor = '#80c1ff';
-        this._selected = Array.from(this.querySelectorAll('dropdown-option')).indexOf(option);
+        option.classList.add('active');
+        // get the index of the current option
+        this._selected = Array.from(this.allOptions).indexOf(option);
         this.querySelector('.selected').textContent = option.textContent;
 
-        this.dispatchEvent(new CustomEvent('change', {detail: {
-            target: this._selected
-        }}));
+        // dispatch a change event
+        this.dispatchEvent(new CustomEvent('change', {
+            detail: {
+                target: option
+            }
+        }));
     }
 
+    /**
+     * Returns the currently selected option element.
+     * @returns {HTMLElement}
+    */
     get selected() {
-        return this.querySelectorAll('dropdown-option')[this._selected];
-    }
-
-    focusNext() {
-        const options = this.querySelectorAll('dropdown-option');
-        let newIdx = this._selected + 1;
-        this.selected = options[newIdx % options.length];
-    }
-
-    focusPrevious() {
-        const options = this.querySelectorAll('dropdown-option');
-        let newIdx = this._selected - 1;
-        this.selected = options[(newIdx + options.length) % options.length];
-    }
-
-    onKeydown(event) {
-        //  TODO: getAllOptions method
-
-        switch (event.keyCode) {
-        case KEY_CODES.TAB:
-        case KEY_CODES.ENTER:
-        case KEY_CODES.ESCAPE:
-            this.closeOptionsPanel();
-            break;
-        case KEY_CODES.HOME:
-            // focus first
-            this.focusElByIdx(0);
-            break;
-        case KEY_CODES.END:
-            // focus last
-            this.focusElByIdx(-1);
-            break;
-        case KEY_CODES.ARROW_UP:
-        case KEY_CODES.ARROW_LEFT:
-            this.focusPrevious();
-            break;
-        case KEY_CODES.ARROW_DOWN:
-        case KEY_CODES.ARROW_RIGHT:
-            this.focusNext();
-            break;
-        }
-        this.scrollToSelectedElement();
+        return this.allOptions[this._selected];
     }
 
     connectedCallback() {
@@ -95,48 +72,121 @@ class GamefaceDropdown extends HTMLElement {
             .then(([loadedTemplate]) => {
                 this.template = loadedTemplate;
                 components.renderOnce(this);
-
+                // make this element focusable
                 this.setAttribute('tabindex', '1');
+                // select the default element
+                this.selected = this.allOptions[this._selected];
                 this.attachEventListeners();
             })
             .catch(err => console.error(err));
     }
 
-    focusElByIdx(idx) {
-        const options = this.querySelectorAll('dropdown-option');
+    /**
+     * Selects the next option element.
+     * Wraps around if it reaches the end of the list.
+    */
+    focusNext() {
+        const options = this.allOptions;
+        let newIdx = this._selected + 1;
+        this.selected = options[newIdx % options.length];
+    }
 
-        if (idx === -1) return this.selected = options[options.length - 1];
+    /**
+     * Selects the previous option element.
+     * Wraps around if it reaches the start of the list.
+    */
+    focusPrevious() {
+        const options = this.allOptions;
+        let newIdx = this._selected - 1;
+        this.selected = options[(newIdx + options.length) % options.length];
+    }
+
+    /**
+     * Called on keydown. Used to handle option selection via the keyboard.
+     * @param {KeyboardEvent} event - the current event object
+    */
+    onKeydown(event) {
+        switch (event.keyCode) {
+            case KEY_CODES.TAB:
+            case KEY_CODES.ENTER:
+            case KEY_CODES.ESCAPE:
+                this.closeOptionsPanel();
+                break;
+            case KEY_CODES.HOME:
+                // focus first
+                this.focusElByIdx(0);
+                break;
+            case KEY_CODES.END:
+                // focus last
+                this.focusElByIdx(-1);
+                break;
+            case KEY_CODES.ARROW_UP:
+            case KEY_CODES.ARROW_LEFT:
+                this.focusPrevious();
+                break;
+            case KEY_CODES.ARROW_DOWN:
+            case KEY_CODES.ARROW_RIGHT:
+                this.focusNext();
+                break;
+        }
+        this.scrollToSelectedElement();
+    }
+
+    /**
+     * Focuses an option element by given index.
+     * If the index is equal to -1 - focus the last element.
+     * If there is no element with at this index - do nothing.
+     * @param {Number} idx - the index of the option element.
+    */
+    focusElByIdx(idx) {
+        const options = this.allOptions;
+
+        if (idx === -1) {
+            this.selected = options[options.length - 1];
+            return;
+        }
+
         if (!options[idx]) return;
+
         this.selected = options[idx];
     }
 
-    attachEventListeners() {
-        const optionsPanel = this.querySelector('.options');
-        const selectedElementPlaceholer = this.querySelector('.selected');
+    /**
+     * Called on click on the select element.
+     * Toggles the options panel, shows the scrollbar and scrolls to
+     * the selected option element.
+    */
+    onClick() {
         const scrollableContainer = this.querySelector('scrollable-container');
 
-        this.addEventListener('keydown', (e) => this.onKeydown(e));
-
-        selectedElementPlaceholer.addEventListener('click', () => {
-            if(this.isOpened) {
-                scrollableContainer.classList.add('hidden');
-                this.isOpened = false;
-                return;
-            };
-
-            this.selected.style.backgroundColor = '#80c1ff';
-            this.isOpened = true;
-            this.scrollToSelectedElement();
-            scrollableContainer.shouldShowScrollbar();
-            scrollableContainer.classList.remove('hidden');
-            this.focus();
-        })
-
-        const options = this.querySelectorAll('dropdown-option');
-        for (let i = 0; i < options.length; i++) {
-            options[i].addEventListener('click', this.onClick);
+        if (this.isOpened) {
+            this.closeOptionsPanel();
+            return;
         }
 
+        this.scrollToSelectedElement();
+        scrollableContainer.shouldShowScrollbar();
+        this.openOptionsPanel();
+    }
+
+    /**
+     * Attaches event listeners.
+    */
+    attachEventListeners() {
+        // handle keyboard
+        this.addEventListener('keydown', (event) => this.onKeydown(event));
+
+        // handle click on the select element
+        const selectedElementPlaceholer = this.querySelector('.selected');
+        selectedElementPlaceholer.addEventListener('click', (event) => this.onClick(event));
+
+        // handle click on the option elements
+        const options = this.querySelectorAll('dropdown-option');
+        for (let i = 0; i < options.length; i++) {
+            options[i].addEventListener('click', (event) => this.onClickOption(event));
+        }
+
+        // handle focus...
         // this.addEventListener('focusout', () => {
         //     console.log('focusout');
         //     this.closeOptionsPanel();
@@ -148,42 +198,66 @@ class GamefaceDropdown extends HTMLElement {
     }
 
     /**
-     * @param {MouseEvent} event
+     * Called when an option element is clicked.
+     * Updates the selected member and closes the options panel.
+     * @param {MouseEvent} event - the current event object.
     */
-    onClick(event) {
-        const target = event.target;
-        this.selected = target;
-
+    onClickOption(event) {
+        this.selected = event.target;
         this.closeOptionsPanel();
     }
 
+    /**
+     * Hides the options panel.
+    */
     closeOptionsPanel() {
-        const scrollableContainer = this.querySelector('scrollable-container');
+        const optionsPanel = this.querySelector('.options-container');
         this.isOpened = false;
-        scrollableContainer.classList.add('hidden');
+        optionsPanel.classList.add('hidden');
     }
 
+    /**
+     * Shows the options panel.
+     * Adds the active class to the currently selected option.
+     * Focuses the dropdown element.
+    */
+    openOptionsPanel() {
+        this.selected.classList.add('active');
+        const optionsPanel = this.querySelector('.options-container');
+        this.isOpened = true;
+        optionsPanel.classList.remove('hidden');
+        this.focus();
+    }
+
+    /**
+     * Sets the selected element of the dropdown and scrolls to it.
+     * @param {DropdownOption} - the option element.
+    */
     setSelected(element) {
-        // TODO: if scrollbar
         this.selected = element;
         this.scrollToSelectedElement();
     }
 
-    // TODO: check if it can be improved
+    /**
+     * Scrolls to the selected option element.
+    */
     scrollToSelectedElement() {
+        // get the scrollable container
         const scrollbleContainer = this.querySelector('.scrollable-container');
+        // get an option element
         const option = this.querySelector('dropdown-option');
+        // get the height of the option element
         const optionSize = option.getBoundingClientRect().height;
 
+        // the scroll position in pixels is equal to the height of the selected
+        // option multiplied by its index
         let scrollInPX = this._selected * optionSize;
         scrollbleContainer.scrollTop = scrollInPX;
         scrollbleContainer.dispatchEvent(new CustomEvent('scroll'));
     }
 }
 
-class DropdownOption extends HTMLElement{}
-
-components.defineCustomElement('dropdown-option', DropdownOption);
+components.defineCustomElement('dropdown-option', class DropdownOption extends HTMLElement {});
 components.defineCustomElement('gameface-dropdown', GamefaceDropdown);
 
 export default GamefaceDropdown;
