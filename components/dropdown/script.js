18ee91425716722bc89a9f5bfb822d15499ce273
venetak <veneta.kochovska93@gmail.com>
2021-02-22 17:01:47 +0200

    Add keyboard support and more focus control

--- components/dropdown/script.js
+++ components/dropdown/script.js
@@ -2,13 +2,26 @@ import components from 'coherent-gameface-components';
 import ScrollableContainer from 'gameface-scrollable-container';
 import template from './template.html';
 import style from './style.css';
- 
+
+const KEY_CODES = {
+    'ARROW_UP': 38,
+    'ARROW_DOWN': 40,
+    'ARROW_RIGHT': 39,
+    'ARROW_LEFT': 37,
+    'END': 35,
+    'HOME': 36,
+    'ENTER': 13,
+    'ESCAPE': 27,
+    'TAB': 9,
+};
+
 class GamefaceDropdown extends HTMLElement {
     constructor() {
         super();
 
         this.selectedOption = null;
         this.isOpened = false;
+        this._selected = 0;
 
         this.template = template;
         components.importStyleTag('gameface-dropdown', style);
@@ -22,13 +35,59 @@ class GamefaceDropdown extends HTMLElement {
         if (this._selected) {
             this.querySelectorAll('dropdown-option')[this._selected].style.backgroundColor = '#fff'
         }
+
         option.style.backgroundColor = '#80c1ff';
         this._selected = Array.from(this.querySelectorAll('dropdown-option')).indexOf(option);
-        this.querySelector('#selected').textContent = option.textContent;
+        this.querySelector('.selected').textContent = option.textContent;
+
+        this.dispatchEvent(new CustomEvent('change', {detail: {
+            target: this._selected
+        }}));
     }
 
     get selected() {
         return this.querySelectorAll('dropdown-option')[this._selected];
+    }
+
+    focusNext() {
+        const options = this.querySelectorAll('dropdown-option');
+        let newIdx = this._selected + 1;
+        this.selected = options[newIdx % options.length];
+    }
+
+    focusPrevious() {
+        const options = this.querySelectorAll('dropdown-option');
+        let newIdx = this._selected - 1;
+        this.selected = options[(newIdx + options.length) % options.length];
+    }
+
+    onKeydown(event) {
+        //  TODO: getAllOptions method
+
+        switch (event.keyCode) {
+        case KEY_CODES.TAB:
+        case KEY_CODES.ENTER:
+        case KEY_CODES.ESCAPE:
+            this.closeOptionsPanel();
+            break;
+        case KEY_CODES.HOME:
+            // focus first
+            this.focusElByIdx(0);
+            break;
+        case KEY_CODES.END:
+            // focus last
+            this.focusElByIdx(-1);
+            break;
+        case KEY_CODES.ARROW_UP:
+        case KEY_CODES.ARROW_LEFT:
+            this.focusPrevious();
+            break;
+        case KEY_CODES.ARROW_DOWN:
+        case KEY_CODES.ARROW_RIGHT:
+            this.focusNext();
+            break;
+        }
+        this.scrollToSelectedElement();
     }
 
     connectedCallback() {
@@ -37,14 +96,26 @@ class GamefaceDropdown extends HTMLElement {
                 this.template = loadedTemplate;
                 components.renderOnce(this);
 
+                this.setAttribute('tabindex', '1');
                 this.attachEventListeners();
             })
             .catch(err => console.error(err));
     }
 
+    focusElByIdx(idx) {
+        const options = this.querySelectorAll('dropdown-option');
+
+        if (idx === -1) return this.selected = options[options.length - 1];
+        if (!options[idx]) return;
+        this.selected = options[idx];
+    }
+
     attachEventListeners() {
-        const selectedElementPlaceholer = this.querySelector('#selected');
-        const scrollableContainer = document.querySelector('scrollable-container');
+        const optionsPanel = this.querySelector('.options');
+        const selectedElementPlaceholer = this.querySelector('.selected');
+        const scrollableContainer = this.querySelector('scrollable-container');
+
+        this.addEventListener('keydown', (e) => this.onKeydown(e));
 
         selectedElementPlaceholer.addEventListener('click', () => {
             if(this.isOpened) {
@@ -53,37 +124,41 @@ class GamefaceDropdown extends HTMLElement {
                 return;
             };
 
-           
+            this.selected.style.backgroundColor = '#80c1ff';
             this.isOpened = true;
-            requestAnimationFrame(() => {
-                requestAnimationFrame(() => {
-                    scrollableContainer.showScrollBar(scrollableContainer.scrollbar);
-                    const scrollableContent = scrollableContainer.querySelector('[name="scrollable-content"]')
-    
-                    scrollableContainer.scrollbar.resize(scrollableContent);
-                    this.scrollToSelectedElement();
-
-                    requestAnimationFrame(() => {
-                        requestAnimationFrame(() => {
-                            scrollableContainer.classList.remove('hidden');
-                        });
-                    });
-                });
-            })
+            this.scrollToSelectedElement();
+            scrollableContainer.shouldShowScrollbar();
+            scrollableContainer.classList.remove('hidden');
+            this.focus();
         })
 
         const options = this.querySelectorAll('dropdown-option');
         for (let i = 0; i < options.length; i++) {
             options[i].addEventListener('click', this.onClick);
         }
+
+        // this.addEventListener('focusout', () => {
+        //     console.log('focusout');
+        //     this.closeOptionsPanel();
+        // });
+
+        // this.addEventListener('focusin', () => {
+        //     console.log('focusin');
+        // });
     }
 
+    /**
+     * @param {MouseEvent} event
+    */
     onClick(event) {
-        // debugger
         const target = event.target;
         this.selected = target;
 
-        const scrollableContainer = document.querySelector('scrollable-container');
+        this.closeOptionsPanel();
+    }
+
+    closeOptionsPanel() {
+        const scrollableContainer = this.querySelector('scrollable-container');
         this.isOpened = false;
         scrollableContainer.classList.add('hidden');
     }
