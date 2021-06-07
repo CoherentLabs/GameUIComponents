b8e876b104b5dcc3a8ce0983acc70b564f80a816
venetak <veneta.kochovska93@gmail.com>
2021-06-07 14:51:21 +0300

    Format css and fix image paths, add missing images and Copyright notice

--- docs/static/components/radial-menu/items-mocking.js
+++ docs/static/components/radial-menu/items-mocking.js
@@ -3,42 +3,42 @@ const itemsModel = {
 		{
 			id: '125',
 			name: 'Pistol X0RG',
-			imagePath: './images/weapon1.png',
+			imagePath: '/images/weapon1.png',
 		},
 		{
 			id: '421',
 			name: 'Shotgun SBud',
-			imagePath: './images/weapon2.png',
+			imagePath: '/images/weapon2.png',
 		},
 		{
 			id: '735',
 			name: 'Assault Brawl D0G',
-			imagePath: './images/weapon3.png',
+			imagePath: '/images/weapon3.png',
 		},
 		{
 			id: '234',
 			name: 'Laser Pistol G0RG',
-			imagePath: './images/weapon4.png',
+			imagePath: '/images/weapon4.png',
 		},
 		{
 			id: '635',
 			name: 'Big Fancy Gun V04',
-			imagePath: './images/weapon5.png',
+			imagePath: '/images/weapon5.png',
 		},
 		{
 			id: '872',
 			name: 'Snippy Flagger',
-			imagePath: './images/weapon6.png',
+			imagePath: '/images/weapon6.png',
 		},
 		{
 			id: '446',
 			name: 'Slide-Action DBud',
-			imagePath: './images/weapon2.png',
+			imagePath: '/images/weapon2.png',
 		},
 		{
 			id: '247',
 			name: 'Laser Blagger',
-			imagePath: './images/weapon4.png',
+			imagePath: '/images/weapon4.png',
 		}
 	]
 };
