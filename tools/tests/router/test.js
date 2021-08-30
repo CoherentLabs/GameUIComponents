f9c4df9de530bac60ea0d043e5c877892294667f
alien1976 <zaloyan@abv.bg>
2021-08-30 11:34:37 +0300

    Added tests

--- tools/tests/router/test.js
+++ tools/tests/router/test.js
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Router, Route, BrowserHistory } from './router.development.js';
+import { Router, Route, BrowserHistory, HashHistory } from './router.development.js';
 
 const NumbersModel = {
     'whole': [1, 2, 3, 4, 5, 6, -7],
@@ -40,9 +40,10 @@ function beforeUnload(callback, params) {
     confirmButton.textContent = 'Yes';
 
     confirmButton.onclick = (e) => {
-        callback.apply(null, params);
+        confirmationDialog.removeChild(confirmButton);
         confirmationDialog.parentElement.removeChild(confirmationDialog);
         confirmButton.style.display = 'none';
+        callback.apply(null, params);
     }
 
     confirmationDialog.appendChild(confirmButton);
@@ -51,11 +52,7 @@ function beforeUnload(callback, params) {
     return false;
 }
 
-const browserHistory = new BrowserHistory();
-Route.use(browserHistory);
-
 function setupPage() {
-
     class Numbers extends HTMLElement {
         constructor() {
             super();
@@ -162,20 +159,44 @@ function setupPage() {
     components.defineCustomElement('not-found-page', NotFound);
     components.defineCustomElement('consonant-page', Consonant);
     components.defineCustomElement('vowel-page', Vowels);
+}
 
-    const lettersRouter = new Router({
+const routerHistories = {
+    BROWSER: new BrowserHistory(),
+    HASH: new HashHistory()
+}
+
+let lettersRouter = null, router = null, currentHistory = null;
+
+function clearPreviousRouter() {
+    if (lettersRouter && router && currentHistory) {
+        lettersRouter.clear();
+        router.clear();
+        lettersRouter = null;
+        router = null;
+        currentHistory = null;
+    }
+}
+
+function setupRouter(history = routerHistories.BROWSER, confirmation = false) {
+    clearPreviousRouter();
+
+    currentHistory = history;
+    Route.use(currentHistory);
+
+    lettersRouter = new Router({
         '/vowel': 'vowel-page',
         '/consonant': 'consonant-page',
     });
 
-    let router = new Router({
+    router = new Router({
         '/': 'home-page',
         '/numbers': 'numbers-page',
         '/documentation': '<div class="documentation">Documentation<div>',
         '/numbers/:type': 'number-page',
         '/letters/:type': lettersRouter,
         '**': 'not-found-page'
-    }, browserHistory, beforeUnload);
+    }, currentHistory, confirmation ? beforeUnload : null);
 }
 
 const routeIdToPageMap = {
@@ -188,16 +209,16 @@ const routeIdToPageMap = {
     'missing': 'not-found-page',
 };
 
-function createAsyncSpec(callback, time = 1000) {
+function createAsyncSpec(callback, frames = 2) {
     return new Promise(resolve => {
-        setTimeout(() => {
+        components.waitForFrames(() => {
             callback();
             resolve();
-        }, time);
+        }, frames)
     });
 }
 
-function setupRouterTestPage() {
+function setupRouterTestPage(history = routerHistories.BROWSER, confirmation = false) {
     const el = document.createElement('div');
     el.innerHTML = template;
     el.className = 'router-test-wrapper';
@@ -211,141 +232,179 @@ function setupRouterTestPage() {
     document.body.appendChild(el);
 
     setupPage();
+    setupRouter(history, confirmation);
     return new Promise(resolve => {
-        setTimeout(() => {
+        components.waitForFrames(() => {
             resolve();
-        }, 1000);
+        }, 2)
     });
 }
 
-describe('Router Component', () => {
-    afterAll(() => {
-        // Since we don't want to replace the whole content of the body using
-        // innerHtml setter, we query only the current custom element and we replace
-        // it with a new one; this is needed because the specs are executed in a random
-        // order and sometimes the component might be left in a state that is not
-        // ready for testing
-        let currentElement = document.querySelector('.router-test-wrapper');
-
-        if (currentElement) {
-            currentElement.parentElement.removeChild(currentElement);
-        }
+async function navigateTo(pageName, confirm) {
+    return await createAsyncSpec(() => {
+        click(document.getElementById(pageName));
+        if (confirm) click(document.querySelector('.confirmButton'));
     });
+}
 
-    beforeEach(function (done) {
-        setupRouterTestPage().then(done).catch(err => console.errror(err));
-    }, 3000);
+function testSuite(title = 'Router Component', routerHistory = routerHistories.BROWSER, confirmation = false) {
+    describe(title, () => {
+        afterAll(() => {
+            // Since we don't want to replace the whole content of the body using
+            // innerHtml setter, we query only the current custom element and we replace
+            // it with a new one; this is needed because the specs are executed in a random
+            // order and sometimes the component might be left in a state that is not
+            // ready for testing
+            let currentElement = document.querySelector('.router-test-wrapper');
 
-    it('Should be rendered', async () => {
-        assert(document.querySelector('gameface-route') !== null, 'The router component is not rendered.');
-    });
-
-    it('Should show home', async () => {
-        click(document.getElementById("home"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['home']) !== null, `Current page is not "home".`);
+            if (currentElement) {
+                currentElement.parentElement.removeChild(currentElement);
+            }
         });
-    });
 
-    it('Should show documentation page that is not a component', async () => {
-        click(document.getElementById("documentation"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector('.documentation')!== null, `Current page is not "documentation".`);
+        beforeEach(function (done) {
+            setupRouterTestPage(routerHistory, confirmation).then(done).catch(err => console.errror(err));
+        }, 3000);
+
+        it('Should be rendered', async () => {
+            return assert(document.querySelector('gameface-route') !== null, 'The router component is not rendered.');
         });
-    });
 
-    it('Should show not found page', async () => {
-        click(document.getElementById("missing"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['missing']) !== null, `Current page is not "missing".`);
-        });
-    });
+        it('Should show home', async () => {
+            await navigateTo('home', confirmation);
 
-    it('Should show numbers', async () => {
-        click(document.getElementById("numbers"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['numbers']) !== null, `Current page is not "numbers"`);
-        });
-    });
-
-    it('Should show whole numbers', async () => {
-        click(document.getElementById("numbers"));
-        return createAsyncSpec(() => {
-            click(document.getElementById("whole"));
-            assert(document.querySelector(routeIdToPageMap['whole']) !== null, 'Current page is not "whole".');
-        }).then(() => {
             return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['home']) !== null, `Current page is not "home".`);
+            });
+        });
+
+        it('Should show documentation page that is not a component', async () => {
+            await navigateTo('documentation', confirmation);
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector('.documentation') !== null, `Current page is not "documentation".`);
+            });
+        });
+
+        it('Should show not found page', async () => {
+            await navigateTo('missing', confirmation);
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['missing']) !== null, `Current page is not "missing".`);
+            });
+        });
+
+        it('Should show numbers', async () => {
+            await navigateTo('numbers', confirmation);
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['numbers']) !== null, `Current page is not "numbers"`);
+            });
+        });
+
+        it('Should show whole numbers', async () => {
+            await navigateTo('numbers', confirmation);
+            await navigateTo('whole', confirmation);
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['whole']) !== null, 'Current page is not "whole".');
                 assert(document.querySelector(routeIdToPageMap['whole']).textContent === NumbersModel.whole.join(','), 'Page "/whole" content is not correct; perhaps it failed to render.');
             });
         });
-    });
 
-    it('Should show rational numbers', async () => {
-        click(document.getElementById("numbers"));
-        await createAsyncSpec(() => {
-            click(document.getElementById("rational"));
-            assert(document.querySelector(routeIdToPageMap['rational']) !== null, 'Current page is not "rational".');
-        }) 
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['rational']).textContent === NumbersModel.rational.join(','), 'Page "/rational" content is not correct; perhaps it failed to render.');
-        });
-    });
+        it('Should show rational numbers', async () => {
+            await navigateTo('numbers', confirmation);
+            await navigateTo('rational', confirmation);
 
-    it('Should show vowel page', async () => {
-        click(document.getElementById("vowel"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['vowel']) !== null, 'Current page is not "vowel".');
-        })
-    });
-
-    it('Should show consonant page', async () => {
-        click(document.getElementById("consonant"));
-        return createAsyncSpec(() => {
-            assert(document.querySelector(routeIdToPageMap['consonant']) !== null, 'Current page is not "consonant".');
-        })
-    });
-
-    xit('Should show warning on back', async () => {
-        click(document.getElementById("home"));
-        click(document.getElementById("numbers"));
-        click(document.getElementById("vowel"));
-
-        await createAsyncSpec(() => {
-            history.back();
-        });
-        click(document.querySelector('.confirmButton'));
-        return createAsyncSpec(() => {
-            assert(history.state.current === '/numbers', 'Current history state is not "/numbers".');
-        });
-    });
-
-    xit('Should show warning on forward', async (done) => {
-        click(document.getElementById("home"));
-        click(document.getElementById("numbers"));
-        click(document.getElementById("vowel"));
-
-        await createAsyncSpec(() => {
-            history.back();
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['rational']) !== null, 'Current page is not "rational".');
+                assert(document.querySelector(routeIdToPageMap['rational']).textContent === NumbersModel.rational.join(','), 'Page "/rational" content is not correct; perhaps it failed to render.');
+            });
         });
 
-        await createAsyncSpec(() => {
-            click(document.querySelector('.confirmButton'));
-            history.forward();
-        })
-        click(document.querySelector('.confirmButton'));
-        return createAsyncSpec(() => {
-            assert(history.state.current === '/letters/vowel', 'Current history state is not "/letters/vowel".');
-        }, 1000);
-    });
+        it('Should show vowel page', async () => {
+            await navigateTo('vowel', confirmation);
 
-    xit('Should navigate to fallback route using pushState', async (done) => {
-        const state = { current: '/wrongpath', id: browserHistory.currentRouteId };
-        const title = 'wrongpath';
-        browserHistory.pushState(state, title, '/wrongpath');
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['vowel']) !== null, 'Current page is not "vowel".');
+            })
+        });
 
-        return createAsyncSpec(() => {
-            assert(history.state.current === '/wrongpath', 'Current history state is not "/wrongpath".');
-            assert(document.querySelector('router-view').textContent === `Can't find this page.`, 'Current page "/wrongpath" content is not correct; perhaps it failed to render.');
+        it('Should show consonant page', async () => {
+            await navigateTo('consonant', confirmation);
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['consonant']) !== null, 'Current page is not "consonant".');
+            })
+        });
+
+        it('Should show warning on back', async () => {
+            await navigateTo('home', confirmation);
+
+            await createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['home']) !== null, 'Current page is not "home".');
+            });
+
+            await navigateTo('numbers', confirmation);
+
+            await createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['numbers']) !== null, 'Current page is not "numbers".');
+            });
+            
+            await navigateTo('vowel', confirmation);
+
+            await createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['vowel']) !== null, 'Current page is not "vowel".');
+                history.back();
+            });
+
+            return createAsyncSpec(async () => {
+                if (confirmation) click(document.querySelector('.confirmButton'));
+                assert(document.querySelector(routeIdToPageMap['numbers']) !== null, 'Current page is not "numbers".');
+            });
+        });
+
+        it('Should show warning on forward', async () => {
+            await navigateTo('home', confirmation);
+            await navigateTo('numbers', confirmation);
+            await navigateTo('vowel', confirmation);
+
+            await createAsyncSpec(() => {
+                history.back();
+            });
+
+            await createAsyncSpec(() => {
+                if (confirmation) click(document.querySelector('.confirmButton'));
+            })
+
+            await createAsyncSpec(() => {
+                history.forward();
+            });
+
+            await createAsyncSpec(() => {
+                if (confirmation) click(document.querySelector('.confirmButton'));
+            })
+
+            return createAsyncSpec(() => {
+                assert(document.querySelector(routeIdToPageMap['vowel']) !== null, 'Current page is not "vowel".');
+            });
+        });
+
+        it('Should navigate to fallback route using pushState', async () => {
+            const state = { current: '/', id: currentHistory.currentRouteId };
+            const title = 'home';
+            Route.history.pushState(state, title, '/');
+
+            return createAsyncSpec(() => {
+                if (confirmation) click(document.querySelector('.confirmButton'));
+
+                assert(document.querySelector(routeIdToPageMap['home']) !== null, 'Current page is not "home".');
+            });
         });
     });
-});
+}
+
+for (let key of Object.keys(routerHistories)) {
+    testSuite(`Router Component - ${key.toLowerCase()}`, routerHistories[key]);
+    testSuite(`Router Component - ${key.toLowerCase()} with confirmation button`, routerHistories[key], true);
+}
