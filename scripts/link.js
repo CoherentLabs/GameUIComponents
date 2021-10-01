b78df3c5a488a1cb74ce65b6995f2daa846ab15b
venetak <veneta.kochovska93@gmail.com>
2021-10-01 14:32:38 +0300

    Link all globally, then locally

--- scripts/link.js
+++ scripts/link.js
@@ -1,34 +1,48 @@
 const path = require('path');
 const fs = require('fs');
-const { execSync, exec } = require('child_process');
+const { execSync } = require('child_process');
 
 const COMPONENTS_PATH = path.join(__dirname, '../components');
+
+function getPackageJSON(component) {
+    const packageJSONPath = path.join(COMPONENTS_PATH, component, 'package.json');
+    if (!fs.lstatSync(packageJSONPath).isFile()) return null;
+    return JSON.parse(fs.readFileSync(packageJSONPath));
+}
 
 function main() {
     const arguments = process.argv.slice(2);
     if (arguments.indexOf('--no-build') === -1) execSync('npm run rebuild', { cwd: path.join(__dirname, '../') });
 
+    // link the components library
     execSync('npm link', { cwd: path.join(__dirname, '../lib') });
     const components = fs.readdirSync(COMPONENTS_PATH);
+
+    // loop all components once to create links to the global node_modules.
+    // This is needed because later we'll link a component's local dependencies
+    // to the global node modules and we need to make sure all components already have links;
+    // otherwise we would have to recursively go through all dependencies, which is slow
+    // and error prone. NPM works like this, but we have an advantage in knowing the list
+    // of all dependencies beforehand - everything in the /components folder and the /lib folder.
     for (let component of components) {
         const links = execSync('npm ls -g --depth=0 --link=true', {encoding: 'utf8'});
+        const packageJSON = getPackageJSON(component);
 
-        const packageJSONPath = path.join(COMPONENTS_PATH, component, 'package.json');
-        if (!fs.lstatSync(packageJSONPath).isFile()) constinue;
-        const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath));
+        if (!packageJSON) continue;
         if (links.match(`${packageJSON.name}`)) continue;
+
         execSync('npm link', { cwd: path.join(COMPONENTS_PATH, component) });
     }
 
+    // loop all components to link their local dependencies to the global
+    // dependencies that we linked in the previous loop.
     for (let component of components) {
-        const packageJSONPath = path.join(COMPONENTS_PATH, component, 'package.json');
-        if (!fs.lstatSync(packageJSONPath).isFile()) continue;
-
-        const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath));
-        if (!packageJSON.dependencies) continue;
+        const packageJSON = getPackageJSON(component);
+        if (!packageJSON || !packageJSON.dependencies) continue;
 
         let componentsDeps = '';
-        for( let dependency of Object.keys(packageJSON.dependencies)) {
+        const dependencies = Object.keys(packageJSON.dependencies);
+        for( let dependency of dependencies) {
             if (!dependency.match(/(coherent)/g)) continue;
             componentsDeps += ` ${dependency}`;
         }
