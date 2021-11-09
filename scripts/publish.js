b87629ed0cdef47572a518d5b9cf91f6e6379b91
venetak <veneta.kochovska93@gmail.com>
2021-11-09 20:24:41 +0200

    Add a publish git workflow

--- scripts/publish.js
+++ scripts/publish.js
@@ -12,21 +12,28 @@ function hasAnUpdate(component) {
     const packageJSON = getPackageJSON(component);
     if (!packageJSON) return false;
 
+    const name = packageJSON.name;
+
+    // if a component doesn't exist in the registry then it must be published
+    if (!execSync(`npm search ${name} --json`).length) return true;
+
     const localVersion = packageJSON.version;
-    const publicVersion = getPublicVersion(packageJSON.name);
+    const publicVersion = getPublicVersion(name);
 
     if(localVersion !== publicVersion) return true;
     return false;
 }
 
 function main() {
-    const components = fs.readdirSync(COMPONENTS_PATH);
-    for (let component of components) {
-        if(hasAnUpdate(component)) {
-            console.log(`--------Component ${component} has new version - ${localVersion}, current is ${publicVersion}.`);
-            // publish
-        }
-    }
+    execSync(`npm publish`, {cwd: path.join(COMPONENTS_PATH, 'test-temp-component')});
+
+    // const components = fs.readdirSync(COMPONENTS_PATH);
+    // for (let component of components) {
+    //     if(hasAnUpdate(component)) {
+    //         console.log(`--------Component ${component} has new version - ${localVersion}, current is ${publicVersion}.`);
+    //         console.log(env.NPM_TOKEN);
+    //     }
+    // }
 }
 
 main();
