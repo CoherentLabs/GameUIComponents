716f6bb966a130ed28905db4cfb32fe9be58293d
venetak <veneta.kochovska93@gmail.com>
2021-11-09 20:52:57 +0200

    Commit temp unpublish

---
+++ scripts/temp_unpublish.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const { execSync } = require('child_process');
+const COMPONENTS_PATH = path.join(__dirname, '../components');
+
+function main() {
+    execSync(`npm unpublish test-temp-component`));
+}
+
+main();
