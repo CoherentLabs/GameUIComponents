59f87a5c6082cc2f9701695c17cfe9b44582adf8
venetak <veneta.kochovska93@gmail.com>
2021-11-09 20:59:23 +0200

    Force the unpublish

--- scripts/temp_unpublish.js
+++ scripts/temp_unpublish.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 const COMPONENTS_PATH = path.join(__dirname, '../components');
 
 function main() {
-    execSync(`npm unpublish test-temp-component`, {encoding: 'utf8'});
+    execSync(`npm unpublish test-temp-component -f`, {encoding: 'utf8'});
 }
 
 main();
