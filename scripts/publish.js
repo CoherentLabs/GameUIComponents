90704d8531a70816ae3ce2837f2da8957ceab054
venetak <veneta.kochovska93@gmail.com>
2021-11-15 08:22:35 +0200

    Check the library and the cli for updated and publish them

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const { getPackageJSON } = require('./helpers');
 const COMPONENTS_PATH = path.join(__dirname, '../components');
+const LIBRARY_PATH = path.join(__dirname, '../');
+const CLI_PATH = path.join(__dirname, '../tools')
 
 function getPublicVersion(package) {
     return execSync(`npm view ${package} version`, {encoding: 'utf8'}).replace('\n', '');
 }
 
-function shouldUpdate(component) {
-    const packageJSON = getPackageJSON(component);
+function shouldUpdate(component, folder = COMPONENTS_PATH) {
+    const packageJSON = getPackageJSON(component, folder);
     if (!packageJSON) return false;
 
     const name = packageJSON.name;
@@ -21,24 +23,29 @@ function shouldUpdate(component) {
     const publicVersion = getPublicVersion(name);
 
     if(localVersion !== publicVersion) {
-        console.log(`Component ${component} has new version - ${localVersion}, current is ${publicVersion}.`);
+        console.log(`Package ${component} has new version - ${localVersion}, current is ${publicVersion}.`);
         return true;
     }
 
     return false;
 }
 
+function publish(component, folder = COMPONENTS_PATH) {
+    try {
+        execSync(`npm publish`, {cwd: path.join(folder, component), encoding: 'utf8'});
+        console.log(`Successfully published ${component}.`);
+    } catch(err) {
+        console.error(err);
+    }
+}
+
 function main() {
+    if (shouldUpdate('lib', LIBRARY_PATH)) publish('lib', LIBRARY_PATH);
+    if (shouldUpdate('cli', CLI_PATH)) publish('cli', CLI_PATH);
+
     const components = fs.readdirSync(COMPONENTS_PATH);
     for (let component of components) {
-        if(shouldUpdate(component)) {
-            try {
-                execSync(`npm publish`, {cwd: path.join(COMPONENTS_PATH, component), encoding: 'utf8'});
-                console.log(`Successfully published ${component}.`);
-            } catch(err) {
-                console.error(err);
-            }
-        }
+        if(shouldUpdate(component)) publish(component);
     }
 }
 
