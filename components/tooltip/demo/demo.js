ec477082558547ffce8e5326e640796fad52bdb0
venetak <veneta.kochovska93@gmail.com>
2021-11-17 18:26:12 +0200

    Add tooltip component

---
+++ components/tooltip/demo/demo.js
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Coherent Labs AD. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import components from 'coherent-gameface-components';
+import tooltip from '../umd/tooltip.development.js'
