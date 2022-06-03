e414d1e5bc8cb3d5494cae6d44868774e7a8269d
Mihail Todorov <mihail@coherent-labs.com>
2022-06-03 14:05:37 +0300

    Add pan and zoom

--- interaction-manager/src/interaction-manager.js
+++ interaction-manager/src/interaction-manager.js
@@ -2,9 +2,14 @@ import keyboard from './lib_components/keyboard';
 import gamepad from './lib_components/gamepad';
 import actions from './lib_components/actions';
 import spatialNavigation from './lib_components/spatial-navigation';
+import draggable from './lib_components/draggable';
+import dropzone from './lib_components/dropzone';
+import rotate from './lib_components/rotate';
+import resize from './lib_components/resize';
+import zoom from './lib_components/zoom';
 
 import IM from './utils/global-object';
 
 IM.init();
 
-export { keyboard, gamepad, actions, spatialNavigation };
+export { keyboard, gamepad, actions, spatialNavigation, draggable, dropzone, rotate, resize, zoom };
---
+++ interaction-manager/src/zoom.js
@@ -0,0 +1,6 @@
+import zoom from './lib_components/zoom';
+import IM from './utils/global-object';
+
+IM.init();
+
+module.exports = zoom;
