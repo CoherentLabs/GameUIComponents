54aeded19ccba3288e02efb9371b98e904ef3807
venetak <veneta.kochovska93@gmail.com>
2023-03-28 17:22:38 +0300

    Use cwd instead of cd

--- tools/cli/tests/start-demo.test.js
+++ tools/cli/tests/start-demo.test.js
@@ -10,7 +10,7 @@ const rimraf = require('rimraf');
 
 const componentFolder = 'create-component-test-folder';
 const componentName = 'test-name';
-const componentFolderPath = path.resolve(path.join(process.cwd(), componentFolder));
+const componentFolderPath = path.resolve(path.join(__dirname, '../', componentFolder));
 const componentSourcePath = path.join(componentFolderPath, componentName);
 
 describe('Start demo test', () => {
@@ -20,14 +20,14 @@ describe('Start demo test', () => {
 
     test('Start a dev server hosting the demo', (done) => {
         // create a component
-        execSync(`node index.js create ${componentName} ./${componentFolder}`, { encoding: 'utf8' });
+        execSync(`node index.js create ${componentName} ./${componentFolder}`, { encoding: 'utf8', stdio: 'inherit' });
 
         // install the component's dependencies
-        execSync(`cd ${componentSourcePath} && npm i`, { encoding: 'utf8' });
+        execSync(`npm i`, { encoding: 'utf8', cwd: componentSourcePath, stdio: 'inherit' });
         // the demo uses the UMD module of the component so we need to build the component first
-        execSync(`cd ${componentSourcePath} && node ../../index.js build`, { encoding: 'utf8' });
+        execSync(`node ../../index.js build`, { encoding: 'utf8', cwd: componentSourcePath, stdio: 'inherit' });
         // serve the demo
-        const subprocess = exec(`cd ${componentSourcePath} && node ../../index.js start:demo`, { encoding: 'utf8' });
+        const subprocess = exec(`node ../../index.js start:demo`, { encoding: 'utf8', cwd: componentSourcePath, stdio: 'inherit' });
         subprocess.on('error', (error) => {
             done(error);
         });
