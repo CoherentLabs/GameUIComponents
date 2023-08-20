a1451e697bb0b77e201a5aaa8390aa19d68e8ffc
venetak <veneta.kochovska93@gmail.com>
2023-08-20 18:59:55 +0300

    Update CLI templates to use latest components

--- tools/cli/templates/projects/vite/index.js
+++ tools/cli/templates/projects/vite/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-import './node_modules/coherent-gameface-components/dist/components.development.js';
-import './node_modules/coherent-gameface-checkbox/dist/checkbox.development.js';
+import 'coherent-gameface-checkbox';
 
 import './node_modules/coherent-gameface-checkbox/coherent-gameface-components-theme.css';
 import './node_modules/coherent-gameface-checkbox/style.css';
