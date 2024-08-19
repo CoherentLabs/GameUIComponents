6b8038bef56daebb1e2c30acb6730115790bb95c
MartinBozhilov-coh <martin.b@coherent-labs.com>
2024-08-19 15:00:11 +0300

    Refactor component state management logic

--- components/toast/script.js
+++ components/toast/script.js
@@ -9,7 +9,7 @@ import template from './template.html';
 const components = new Components();
 const BaseComponent = components.BaseComponent;
 const TOAST_POSITIONS = ['top left', 'top right', 'bottom left', 'bottom right', 'top center', 'bottom center'];
-const classPrefix = 'guic-toast-';
+const CLASS_PREFIX = 'guic-toast-';
 let containersCreated = false;
 let animationEventAttached = false;
 
@@ -17,16 +17,33 @@ let animationEventAttached = false;
  * Class definition of the gameface toast custom element
  */
 class GamefaceToast extends BaseComponent {
+    // eslint-disable-next-line require-jsdoc
+    static get observedAttributes() { return ['gravity', 'position', 'target', 'timeout']; }
     /* eslint-disable require-jsdoc */
     constructor() {
         super();
         this.template = template;
+
         this.init = this.init.bind(this);
+
         this.hide = this.hide.bind(this);
         this.show = this.show.bind(this);
+
         this.hideTimeOut = null;
-        this._gravity = 'top';
-        this._position = 'left';
+
+        this.stateSchema = {
+            gravity: { type: ['string'] },
+            position: { type: ['string'] },
+            target: { type: ['string', 'object'] },
+            timeout: { type: ['number'] },
+        };
+
+        this.state = {
+            gravity: 'top',
+            position: 'left',
+            target: null,
+            timeout: 0,
+        };
     }
 
     get message() {
@@ -37,60 +54,53 @@ class GamefaceToast extends BaseComponent {
         this._messageSlot.innerHTML = value;
     }
 
-    get targetElement() {
-        return this._targetElement;
+    get target() {
+        return this.state.target;
     }
 
-    set targetElement(element) {
-        this._targetElement = element;
+    set target(element) {
+        this.setAttribute('target', element);
     }
 
     get position() {
-        return this._position;
+        return this.state.position;
     }
 
     set position(value) {
-        this._position = value;
         this.setAttribute('position', value);
     }
 
     get gravity() {
-        return this._gravity;
+        return this.state.gravity;
     }
 
     set gravity(value) {
-        this._gravity = value;
         this.setAttribute('gravity', value);
     }
 
     init(data) {
         this.setupTemplate(data, () => {
             components.renderOnce(this);
+            if (this.hasAttribute('gravity')) this.updateAttributeState('gravity', this.getAttribute('gravity'));
+            if (this.hasAttribute('position')) this.updateAttributeState('position', this.getAttribute('position'));
+            if (this.hasAttribute('timeout')) this.updateAttributeState('timeout', parseInt(this.getAttribute('timeout')) || 0);
+            if (this.hasAttribute('target')) this.updateAttributeState('target', this.getAttribute('target'));
+
+            this._messageSlot = this.querySelector('.guic-toast-message').firstElementChild;
+
+            if (!containersCreated) this.createToastContainers();
             // attach event handlers here
             this.attachEventListeners();
-            this._messageSlot = this.querySelector('.guic-toast-message').firstElementChild;
         });
     }
 
     connectedCallback() {
-        this.gravity = this.getAttribute('gravity') || this._gravity;
-        this.position = this.getAttribute('position') || this._position;
-        this.timeout = this.getAttribute('timeout') || 0;
-        this.elementSelector = this.getAttribute('target');
-        this._targetElement = this._targetElement || document.querySelector(this.elementSelector);
-
-        if (!containersCreated) this.createToastContainers();
-
         components.loadResource(this)
             .then(this.init)
             .catch(err => console.error(err));
     }
 
     attachEventListeners() {
-        if (this._targetElement ) {
-            this._targetElement.addEventListener('click', this.show);
-        }
-
         if (!animationEventAttached) {
             document.addEventListener('animationend', (event) => {
                 if (event.animationName === 'guic-toast-fade-out') event.target.parentElement.removeChild(event.target);
@@ -101,6 +111,62 @@ class GamefaceToast extends BaseComponent {
     /* eslint-enable require-jsdoc */
 
     /**
+     * Custom element lifecycle method. Called when an attribute is changed.
+     * @param {string} name
+     * @param {string} oldValue
+     * @param {string|boolean|array} newValue
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (!this.isRendered) return;
+
+        this.updateAttributeState(name, newValue);
+    }
+
+    /**
+     * Will update the state properties linked with the checkbox attributes
+     * @param {string} name
+     * @param {string|boolean|array} value
+     */
+    updateAttributeState(name, value) {
+        switch (name) {
+            case 'position':
+            case 'gravity':
+            case 'timeout':
+                this.updateState(name, value);
+                break;
+            case 'target':
+                this.updateTargetState(name, value);
+                break;
+        }
+    }
+
+    /**
+     * Update the toasts's state.
+     * @param {string} name - the name of the prop
+     * @param {string | boolean} value - the value of the the prop
+     * @returns {void}
+     */
+    updateState(name, value) {
+        if (!this.isStatePropValid(name, value)) return;
+        this.state[name] = value;
+    }
+
+    /**
+     * Update the toasts's state.
+     * @param {string} name - the name of the prop
+     * @param {string | boolean} value - the value of the the prop
+     * @returns {void}
+     */
+    updateTargetState(name, value) {
+        if (!this.isStatePropValid(name, value)) return;
+
+        if (value instanceof HTMLElement === false) {
+            this.state.target = document.querySelector(value);
+        }
+        this.state.target.addEventListener('click', this.show);
+    }
+
+    /**
      * Creates containers for all possible toast positions
     */
     createToastContainers() {
@@ -108,7 +174,7 @@ class GamefaceToast extends BaseComponent {
         TOAST_POSITIONS.forEach((containerPosition) => {
             const [vertical, horizontal] = containerPosition.split(' ');
             const toastContainer = document.createElement('div');
-            toastContainer.classList.add('guic-toast-container', `${classPrefix}${vertical}`, `${classPrefix}${horizontal}`);
+            toastContainer.classList.add('guic-toast-container', `${CLASS_PREFIX}${vertical}`, `${CLASS_PREFIX}${horizontal}`);
             body.appendChild(toastContainer);
         });
 
@@ -121,7 +187,7 @@ class GamefaceToast extends BaseComponent {
      * @param {string} position - left, right, center
     */
     appendToastToContainer(gravity, position) {
-        const container = document.querySelector(`.guic-toast-container.${classPrefix}${gravity}.${classPrefix}${position}`);
+        const container = document.querySelector(`.guic-toast-container.${CLASS_PREFIX}${gravity}.${CLASS_PREFIX}${position}`);
 
         if (container) {
             container.appendChild(this);
@@ -135,7 +201,7 @@ class GamefaceToast extends BaseComponent {
      * Displays the toast
      */
     show() {
-        this.appendToastToContainer(this.gravity, this.position);
+        this.appendToastToContainer(this.state.gravity, this.state.position);
         this.handleTimeOut();
         this.handleCloseButton();
         this.classList.add('guic-toast-show');
@@ -146,7 +212,7 @@ class GamefaceToast extends BaseComponent {
      * Hides the toast
      */
     hide() {
-        if (this.isConnected) this.classList.add('guic-toast-hide');
+        if (this.isRendered) this.classList.add('guic-toast-hide');
     }
 
     /**
@@ -155,8 +221,8 @@ class GamefaceToast extends BaseComponent {
     handleTimeOut() {
         if (this.hideTimeOut) clearTimeout(this.hideTimeOut);
 
-        if (this.timeout > 0) {
-            this.hideTimeOut = setTimeout(this.hide, this.timeout);
+        if (this.state.timeout > 0) {
+            this.hideTimeOut = setTimeout(this.hide, this.state.timeout);
         }
     }
 
